6a2defcfe787854225bb946f7dfaa7950528f9f3
kamangir <arash@kamangir.net>
2025-06-27 12:05:16 +0330

    pipeline fix, hopefully - kamangir/bolt#746

--- bluer_algo/__init__.py
+++ bluer_algo/__init__.py
@@ -4,7 +4,7 @@ ICON = "🪄"
 
 DESCRIPTION = f"{ICON} AI Algo."
 
-VERSION = "4.62.1"
+VERSION = "4.63.1"
 
 REPO_NAME = "bluer-algo"
 
--- bluer_algo/tests/fruits_360.py
+++ bluer_algo/tests/fruits_360.py
@@ -3,14 +3,16 @@ import pytest
 
 from bluer_objects import path
 
-from bluer_algo.env import BLUER_ALGO_FRUITS_360_REPO_ADDRESS
-from bluer_journal.classes.journal import journal
+from bluer_algo.env import (
+    BLUER_ALGO_FRUITS_360_REPO_ADDRESS,
+    BLUER_ALGO_FRUITS_360_REPO_PATH,
+)
 
 
 @pytest.fixture
 def fruits_360_checkout():
-    if not path.exists(journal.path):
+    if not path.exists(BLUER_ALGO_FRUITS_360_REPO_PATH):
         git.Repo.clone_from(
             BLUER_ALGO_FRUITS_360_REPO_ADDRESS,
-            journal.path,
+            BLUER_ALGO_FRUITS_360_REPO_PATH,
         )
