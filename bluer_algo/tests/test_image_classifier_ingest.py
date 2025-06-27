320340eb6c90aeb81afd9cbc080d7560ec06a8ad
kamangir <arash@kamangir.net>
2025-06-27 09:16:24 +0330

    pipeline fix, hopefully - kamangir/bolt#746

--- bluer_algo/tests/test_image_classifier_ingest.py
+++ bluer_algo/tests/test_image_classifier_ingest.py
@@ -2,8 +2,14 @@ import pytest
 
 from bluer_objects.objects import unique_object
 
+from bluer_algo.image_classifier.ingest import ingest
+
 
 def test_image_classifier_ingest():
     object_name = unique_object("test_image_classifier_ingest")
 
-    assert True
+    success = ingest(
+        object_name=object_name,
+    )
+
+    assert success
