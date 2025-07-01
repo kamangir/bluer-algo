373ee1f1d579e11d946fb3a38a448180a54ce371
kamangir <arash@kamangir.net>
2025-07-01 19:08:08 +0330

    ++ - kamangir/bolt#746

--- bluer_algo/tests/test_image_classifier_model_train.py
+++ bluer_algo/tests/test_image_classifier_model_train.py
@@ -1,6 +1,7 @@
 import pytest
 
 from bluer_objects import storage
+from bluer_objects.objects import unique_object
 
 from bluer_algo.image_classifier.model.train import train
 from bluer_algo.env import BLUER_ALGO_FRUITS_360_TEST_DATASET
@@ -13,10 +14,13 @@ from bluer_algo.env import BLUER_ALGO_FRUITS_360_TEST_DATASET
     ],
 )
 def test_image_classifier_model_train(dataset_object_name: str):
+    model_object_name = unique_object("test_image_classifier_model_train")
+
     assert storage.download(object_name=dataset_object_name)
 
     success = train(
         dataset_object_name=dataset_object_name,
+        model_object_name=model_object_name,
     )
 
     assert success
