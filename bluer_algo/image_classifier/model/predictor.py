ec4980c87a06fa6dc22cb9c76ea9cbbbcbd0c097
kamangir <arash@kamangir.net>
2025-07-02 12:22:08 +0330

    ++ - kamangir/bolt#746

--- bluer_algo/image_classifier/model/predictor.py
+++ bluer_algo/image_classifier/model/predictor.py
@@ -4,9 +4,10 @@ import numpy as np
 import time
 from PIL import Image
 from torchvision import transforms
+import matplotlib.pyplot as plt
 
 from bluer_options import string
-from bluer_objects import objects
+from bluer_objects import objects, file
 from bluer_objects.metadata import get_from_object
 
 from bluer_algo.image_classifier.model.model import TinyCNN
@@ -18,6 +19,8 @@ class ImageClassifierPredictor:
         self.class_count = 0
         self.dict_of_classes = {}
         self.model = None
+        self.transform = None
+        self.shape = []
 
     @staticmethod
     def load(object_name: str) -> Tuple[bool, "ImageClassifierPredictor"]:
@@ -39,7 +42,7 @@ class ImageClassifierPredictor:
         if "dataset" not in predictor.metadata:
             logger.error("dataset not found.")
             return False, predictor
-        for thing in ["class_count", "classes"]:
+        for thing in ["class_count", "classes", "shape"]:
             if thing not in predictor.metadata["dataset"]:
                 logger.error(f"dataset.{thing} not found.")
                 return False, predictor
@@ -59,6 +62,9 @@ class ImageClassifierPredictor:
             )
         )
 
+        predictor.shape = predictor.metadata["dataset"]["shape"]
+        logger.info("shape: {}".format(predictor.shape))
+
         predictor.filename = objects.path_of(
             object_name=predictor.object_name,
             filename="model.pth",
@@ -77,6 +83,14 @@ class ImageClassifierPredictor:
             logger.error(e)
             return False, predictor
 
+        # Apply same transform as training
+        predictor.transform = transforms.Compose(
+            [
+                transforms.Resize((predictor.shape[0], predictor.shape[1])),
+                transforms.ToTensor(),
+            ]
+        )
+
         return True, predictor
 
     def predict(
@@ -89,11 +103,11 @@ class ImageClassifierPredictor:
         # np_img is shape (H, W, 3) in RGB
         if not isinstance(image, np.ndarray):
             logger.error(f"{image.__class__.__name__} not supported.")
-            return False, 0, {}
+            return False, -1, {}
 
-        if not (image.ndim == 3 and image.shape[2] == 3):
+        if not (image.ndim == 3 and image.shape[2] == self.shape[2]):
             logger.error("color image expected.")
-            return False, 0, {}
+            return False, -1, {}
 
         elapsed_time = time.time()
 
@@ -101,14 +115,9 @@ class ImageClassifierPredictor:
             # Convert to PIL for transforms
             image_ = Image.fromarray(image.astype("uint8"))
 
-            # Apply same transform as training
-            transform = transforms.Compose(
-                [
-                    transforms.Resize((100, 100)),
-                    transforms.ToTensor(),
-                ]
-            )
-            input_tensor = transform(image_).unsqueeze(0)  # Shape: [1, 3, 100, 100]
+            input_tensor = self.transform(image_).unsqueeze(
+                0
+            )  # Shape: [1, 3, 100, 100]
 
             with torch.no_grad():
                 output = self.model(input_tensor)
@@ -117,19 +126,23 @@ class ImageClassifierPredictor:
             elapsed_time = time.time() - elapsed_time
         except Exception as e:
             logger.error(e)
-            return False, 0, {}
+            return False, -1, {}
 
         if log:
             logger.info(
-                "prediction: {} [#{}] {}- took {}".format(
+                "prediction: {} [#{}]{}- took {}".format(
                     self.dict_of_classes[predicted_class],
                     predicted_class,
                     (
-                        " (correct) "
-                        if class_index == predicted_class or class_index == -1
-                        else "<> {} [#{}] ".format(
-                            self.dict_of_classes[class_index],
-                            class_index,
+                        ""
+                        if class_index == -1
+                        else (
+                            " ✅ "
+                            if class_index == predicted_class
+                            else "<❗️> {} [#{}]".format(
+                                self.dict_of_classes[class_index],
+                                class_index,
+                            )
                         )
                     ),
                     string.pretty_duration(
@@ -139,6 +152,41 @@ class ImageClassifierPredictor:
                     ),
                 )
             )
+
+        if object_name:
+            plt.figure(figsize=(5, 5))
+            plt.imshow(image)
+            plt.title(
+                "prediction: {} [#{}] {}- took {}".format(
+                    self.dict_of_classes[predicted_class],
+                    predicted_class,
+                    (
+                        ""
+                        if class_index == -1
+                        else (
+                            " (correct) "
+                            if class_index == predicted_class
+                            else "<> {} [#{}] ".format(
+                                self.dict_of_classes[class_index],
+                                class_index,
+                            )
+                        )
+                    ),
+                    string.pretty_duration(
+                        elapsed_time,
+                        include_ms=True,
+                        short=True,
+                    ),
+                )
+            )
+            plt.axis("off")
+            if not file.save_fig(
+                objects.path_of(
+                    object_name=object_name,
+                    filename="prediction.png",
+                )
+            ):
+                return False, -1, {}
 
         return (
             True,
