374fbd30ac98e40f7aa2378db34a5055571e2b18
kamangir <arash@kamangir.net>
2025-07-15 20:10:47 +0330

    ++ - kamangir/bolt#746

--- sandbox/mean-cam-shift/meanshift-v4.py
+++ sandbox/mean-cam-shift/meanshift-v4.py
@@ -66,13 +66,10 @@ else:
 track_window = (x, y, w, h)
 
 tracker = MeanShiftTracker()
-
-# set up the ROI for tracking
-roi = frame[y : y + h, x : x + w]
-hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-mask = cv.inRange(hsv_roi, np.array((0.0, 60.0, 32.0)), np.array((180.0, 255.0, 255.0)))
-roi_hist = cv.calcHist([hsv_roi], [0], mask, [180], [0, 180])
-cv.normalize(roi_hist, roi_hist, 0, 255, cv.NORM_MINMAX)
+tracker.start(
+    frame=frame,
+    track_window=track_window,
+)
 
 # Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
@@ -90,7 +87,7 @@ while 1:
 
     if ret == True:  # pylint: disable=C0121
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        dst = cv.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+        dst = cv.calcBackProject([hsv], [0], tracker.roi_hist, [0, 180], 1)
 
         # apply meanshift to get the new location
         ret, track_window = cv.meanShift(dst, track_window, term_crit)
