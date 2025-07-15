fc8df5aa81839780003a5b5f216ce5fe219f3648
kamangir <arash@kamangir.net>
2025-07-15 13:35:02 +0330

    pipeline fix, hopefully - kamangir/bolt#746

--- sandbox/mean-cam-shift/camshift.py
+++ sandbox/mean-cam-shift/camshift.py
@@ -32,7 +32,7 @@ term_crit = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1)
 while 1:
     ret, frame = cap.read()
 
-    if ret == True:
+    if ret == True:  # pylint: disable=C0121
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         dst = cv.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
 
