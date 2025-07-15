fc500549ca957695398f678e18aba3b463692b44
kamangir <arash@kamangir.net>
2025-07-15 14:10:30 +0330

    pipeline fix, hoprfully - kamangir/bolt#746

--- bluer_algo/__init__.py
+++ bluer_algo/__init__.py
@@ -4,7 +4,7 @@ ICON = "🪄"
 
 DESCRIPTION = f"{ICON} AI Algo."
 
-VERSION = "4.266.1"
+VERSION = "4.267.1"
 
 REPO_NAME = "bluer-algo"
 
--- sandbox/mean-cam-shift/meanshift-v3.py
+++ sandbox/mean-cam-shift/meanshift-v3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import argparse
+import sys
 
 from bluer_algo.logger import logger
 
