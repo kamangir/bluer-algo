93fc74441f8e7ebd7de8b9fa847aa837f4d6ffe9
kamangir <arash@kamangir.net>
2025-07-16 11:03:53 +0330

    ++ - kamangir/bolt#746

--- bluer_algo/tracker/classes/camshift.py
+++ bluer_algo/tracker/classes/camshift.py
@@ -22,11 +22,28 @@ class CamShiftTracker(GenericTracker):
         # apply camshift to get the new location
         ret, track_window = cv2.CamShift(dst, track_window, self.term_crit)
 
+        x, y, w, h = track_window
+        self.history.append(
+            (
+                x + w // 2,
+                y + h // 2,
+            )
+        )
+
         # Draw it on image
         output_image = np.array([])
         if self.with_gui or log:
             pts = cv2.boxPoints(ret)
             pts = np.intp(pts)
             output_image = cv2.polylines(frame, [pts], True, 255, 2)
+
+            for i in range(len(self.history) - 1):
+                cv2.line(
+                    output_image,
+                    self.history[i],
+                    self.history[i + 1],
+                    color=(0, 255, 0),
+                    thickness=2,
+                )
 
         return ret, track_window, output_image
--- bluer_algo/tracker/classes/meanshift.py
+++ bluer_algo/tracker/classes/meanshift.py
@@ -22,11 +22,26 @@ class MeanShiftTracker(GenericTracker):
         # apply meanshift to get the new location
         ret, track_window = cv2.meanShift(dst, track_window, self.term_crit)
 
+        x, y, w, h = track_window
+        self.history.append(
+            (
+                x + w // 2,
+                y + h // 2,
+            )
+        )
+
         # Draw it on image
         output_image = np.array([])
         if self.with_gui or log:
-            x, y, w, h = track_window
-
             output_image = cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
+
+            for i in range(len(self.history) - 1):
+                cv2.line(
+                    output_image,
+                    self.history[i],
+                    self.history[i + 1],
+                    color=(0, 255, 0),
+                    thickness=2,
+                )
 
         return ret, track_window, output_image
