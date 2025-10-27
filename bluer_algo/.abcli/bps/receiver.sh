9349670032240e794d738520beaadac4705f1bba
kamangir <arash@kamangir.net>
2025-10-27 13:55:13 +0330

    ++ - kamangir/bolt#746

--- bluer_algo/.abcli/bps/receiver.sh
+++ bluer_algo/.abcli/bps/receiver.sh
@@ -5,15 +5,11 @@ function bluer_algo_bps_receiver() {
     local use_python=$(bluer_ai_option_int "$options" python 1)
 
     if [[ "$use_python" == 1 ]]; then
-        local do_download=$(bluer_ai_option_int "$options" download 1)
         local do_upload=$(bluer_ai_option_int "$options" upload 0)
 
         local object_name=$(bluer_ai_clarify_object $2 bps-receiver-$(bluer_ai_string_timestamp))
 
         bluer_ai_log "starting bps receiver -> $object_name ..."
-
-        [[ "$do_download" == 1 ]] &&
-            bluer_objects_download - $object_name
 
         bluer_ai_eval ,$options \
             sudo -E \
@@ -24,9 +20,7 @@ function bluer_algo_bps_receiver() {
         [[ $? -ne 0 ]] && return 1
 
         [[ "$do_upload" == 1 ]] &&
-            bluer_objects_upload \
-                filename=metadata.yaml \
-                $object_name
+            bluer_objects_upload - $object_name
 
         return 0
     else
