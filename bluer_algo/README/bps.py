2b9fa42deca1c7df885e20f01d0be849df6e9cee
kamangir <arash@kamangir.net>
2025-10-27 10:08:36 +0330

    ++ - kamangir/bolt#746

--- bluer_algo/README/bps.py
+++ bluer_algo/README/bps.py
@@ -14,10 +14,10 @@ docs = [
         "cols": 2,
         "items": ImageItems(
             {
-                f"{assets2}/2.png": "",
-                f"{assets2}/0.png": "",
-                f"{assets2}/1.png": "",
-                f"{assets2}/3.png": "",
+                f"{assets2}/03.png": "",
+                f"{assets2}/01.png": "",
+                f"{assets2}/02.png": "",
+                f"{assets2}/04.png": "",
             }
         ),
     }
