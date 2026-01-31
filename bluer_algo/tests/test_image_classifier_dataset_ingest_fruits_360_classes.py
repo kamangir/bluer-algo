4f4d83bd7eb5929104da7e7c57c62cef36657a03
kamangir <arash@kamangir.net>
2026-01-31 16:12:47 +0330

    start of back-online-2026-01-31-16-12-19-gsd1sd 🌀

--- bluer_algo/tests/test_image_classifier_dataset_ingest_fruits_360_classes.py
+++ bluer_algo/tests/test_image_classifier_dataset_ingest_fruits_360_classes.py
@@ -1,10 +1,7 @@
-import pytest
-
 from bluer_algo.image_classifier.dataset.ingest.fruits_360.classes import get_classes
 from bluer_algo.tests.fruits_360 import fruits_360_checkout
 
 
-@pytest.mark.skip(reason="no arvan on github + repo not available on local")
 def test_image_classifier_dataset_ingest_fruits_360_classes(fruits_360_checkout):
     dict_of_classes = get_classes(class_count=10)
 
