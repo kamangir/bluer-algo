7e1c204d7ff8b4dd5adfb038931c3f705ce7aefb
kamangir <arash@kamangir.net>
2026-02-08 13:33:51 +0330

    wip

--- bluer_algo/bps/bluetooth/start.py
+++ bluer_algo/bps/bluetooth/start.py
@@ -1,7 +1,11 @@
-from blueness import module
+from typing import List
 
-from bluer_plugin import NAME
-from bluer_plugin.logger import logger
+from blueness import module
+from bluer_options import env
+from bluer_objects.host import shell
+
+from bluer_algo import NAME
+from bluer_algo.logger import logger
 
 
 NAME = module.name(__file__, NAME)
@@ -15,6 +19,28 @@ def start(verbose: bool) -> bool:
         )
     )
 
-    logger.info("🪄")
+    list_of_commands: List[str] = []
 
-    return True
+    logger.info("starting bluetooth...")
+
+    if env.abcli_is_rpi4 == "true":
+        logger.info("rpi4b detected.")
+        list_of_commands += [
+            "sudo rfkill unblock bluetooth",
+            "sudo hciconfig hci0 up",
+        ]
+
+    list_of_commands += ["sudo systemctl start bluetooth"]
+
+    if verbose:
+        list_of_commands += ["sudo systemctl status --no-pager bluetooth"]
+
+    list_of_commands += [
+        "sudo bluetoothctl power on",
+        "sudo bluetoothctl discoverable on",
+    ]
+
+    if verbose:
+        list_of_commands += ["sudo bluetoothctl show"]
+
+    return all(shell(command=command) for command in list_of_commands)
